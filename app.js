be73fad5feaa16d8103248cb6611b712d74c676c
Rosy Zimmerli <rachel.zimmerli@my.southcentral.edu>
2020-05-01 19:48:34 -0500

    Set up express server

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
+
+var port = 8080;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
